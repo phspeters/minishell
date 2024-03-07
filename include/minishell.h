5f155591a92db10a6705ef4f84db15cb01f28ce1
Pedro Henrique De Souza Peters <pehenri2@c3r10p3.42sp.org.br>
2024-03-07 16:26:56 -0300

    starting project

---
+++ include/minishell.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pehenri2 <pehenri2@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/28 15:56:37 by pehenri2          #+#    #+#             */
+/*   Updated: 2024/03/07 16:04:09 by pehenri2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+enum e_meta
+{
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC,
+	WORD,
+};
+
+#endif
